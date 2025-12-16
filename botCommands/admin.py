fdfcba76be11782f11b3aae4d2a07e414c3a20aa
Focky <72989848+Fockys@users.noreply.github.com>
2025-12-16 16:12:56 +0000

    smol fix

--- botCommands/admin.py
+++ botCommands/admin.py
@@ -102,7 +102,7 @@ class adminCog(commands.Cog):
         
         role = interaction.guild.get_role(MemberRole)
         try:
-            targetDiscID = self.client.dbHan.getStudentFromAstonID(target)
+            targetDiscID = self.client.dbHan.getStudentFromAstonID(target)[1]
             target = botClient.fetch_user(targetDiscID)
             await target.add_roles(role)
         except Exception as e:
